ea464e5a0c06375838312c2e32d2b65d6065b025
ch-shubham <shubham128166@gmail.com>
2022-05-05 19:11:13 +0530

    Queue using array with dynamic size.(Enhanced version of previous commit)

--- Queue/QueueUsingArray.h
+++ Queue/QueueUsingArray.h
@@ -27,8 +27,24 @@ class QueueUsingArray{
     // insert element
     void enqueue(T element){
       if(size == capacity){
-        cout<<"Queue is full and no extra elements can be inserted."<<endl;
-        return;
+        T *newData = new T[2*capacity];
+        int j = 0;
+        for(int i = firstIndex; i<capacity; i++){
+          newData[j] = data[i];
+          j++;
+        }
+        for(int i = 0; i < firstIndex; i++ ){
+          newData[j] = data[i];
+          j++;
+        }
+        delete [] data;
+        data = newData;
+        firstIndex = 0;
+        nextIndex = capacity;
+        capacity *= 2;
+
+        // cout<<"Queue is full and no extra elements can be inserted."<<endl;
+        // return;
       }
       data[nextIndex] = element;
       nextIndex = (nextIndex + 1) % capacity;
