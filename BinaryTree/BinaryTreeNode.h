a7b869957f707e31394dae1614b128f5f0c482c4
ch-shubham <shubham128166@gmail.com>
2022-05-12 18:09:42 +0530

    implementing BinaryTreeNode class and BinaryUse to use the Node Class

---
+++ BinaryTree/BinaryTreeNode.h
@@ -0,0 +1,18 @@
+template <typename T>
+class BinaryTreeNode(){
+  public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    BinaryTreeNode(T data){
+      this->data = data;
+      left = NULL;
+      right = NULL;
+    }
+
+    ~BinaryTreeNode(){
+      delete left; // delete NULL will do nothing so no need to check if left or right is NULL or not.
+      delete right;  // delete NULL will do nothing so no need to check if left or right is NULL or not.
+    }
+}
